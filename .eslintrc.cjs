63486340aa807eab25194ecebc7b32ab23a994fc
Xyphuz <hansen0715.2@gmail.com>
2023-04-02 19:31:43 +0800

    chore: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: [
+		'plugin:svelte/recommended',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	plugins: ['@typescript-eslint', 'prettier'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'no-inner-declarations': 'off',
+		semi: ['error', 'always'],
+	},
+};
