c185bd4c79f25f5074b1a080fa2756369ec23142
Xyphuz <hansen0715.2@gmail.com>
2023-04-23 22:32:09 +0800

    fix: missing prerender for `add bookmark` page

--- src/routes/bookmarks/add/+page.ts
+++ src/routes/bookmarks/add/+page.ts
@@ -2,6 +2,8 @@ import { superValidate } from "sveltekit-superforms/server";
 import { schemas } from "../../../api/api.client";
 import { api } from "../../../api";
 
+export const prerender = true;
+
 export const load = async () => {
 	const form = await superValidate(schemas.BookmarkPayload);
 	const tags = await api.getTags();
