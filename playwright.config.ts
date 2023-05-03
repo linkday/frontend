a5d7d3de87af81ee667c17c660591ab3ed5d9f6e
Xyphuz <hansen0715.2@gmail.com>
2023-05-04 00:04:52 +0800

    fix: use pnpm and playwright with deps in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: "npm run build && npm run preview",
+		command: "pnpm build && pnpm preview",
 		port: 4173,
 	},
 	testDir: "tests",
