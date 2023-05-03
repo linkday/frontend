e61fac396a081e9e2d70923ef639070917f8745f
Xyphuz <hansen0715.2@gmail.com>
2023-05-04 00:11:08 +0800

    fix: debug using browser in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: "pnpm build && pnpm preview",
+		command: "npm run build && npm run preview",
 		port: 4173,
 	},
 	testDir: "tests",
