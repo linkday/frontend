fe2dd2da1d5b5e6f2b3714d28269be7374a21826
Xyphuz <hansen0715.2@gmail.com>
2023-05-04 00:23:33 +0800

    fix: use absolute path to find tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,12 @@
 import { PlaywrightTestConfig } from "@playwright/test";
+import path from "path";
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: "npm run build && npm run preview",
 		port: 4173,
 	},
-	testDir: "tests",
+	testDir: path.join(__dirname, "tests"),
 	fullyParallel: true,
 };
 
