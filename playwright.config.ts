4b638fd0fb7494d34c4e7eb93a496b3b80f4f53e
Xyphuz <hansen0715.2@gmail.com>
2023-05-04 00:31:27 +0800

    fix: rename from test.ts to test.spec.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,11 @@
 import { PlaywrightTestConfig } from "@playwright/test";
-import path from "path";
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: "npm run build && npm run preview",
 		port: 4173,
 	},
-	testDir: path.join(__dirname, "tests"),
+	testDir: "tests",
 	fullyParallel: true,
 };
 
