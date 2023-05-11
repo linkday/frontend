ff86a9bde2090e6bdcbd580b68883a78b9eb3e23
Xyphuz <hansen0715.2@gmail.com>
2023-05-11 16:39:47 +0800

    fix: use logout endpoint to invalidate credentials

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import { browser } from "$app/environment";
+	import { api } from "../../api";
 
 	if (browser) {
-		goto("/", {
-			invalidateAll: true,
-		});
+		api
+			.logout(undefined, {
+				withCredentials: true,
+			})
+			.then(() => {
+				goto("/", {
+					invalidateAll: true,
+				});
+			});
 	}
 </script>
