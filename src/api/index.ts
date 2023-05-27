ba1e4a31595aad78b30a59687981e39b328d41fc
Xyphuz <hansen0715.2@gmail.com>
2023-05-28 01:08:03 +0800

    fix: add zodios workaround back

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import { createApiClient } from "./api.gen";
 import { env } from "$env/dynamic/public";
 
-export const api = createApiClient(env.PUBLIC_API_PREFIX);
+// This is a workaround for Zodios, which will check if the API url is valid during build time
+export const api = createApiClient(env.PUBLIC_API_PREFIX || "https://www.google.com");
